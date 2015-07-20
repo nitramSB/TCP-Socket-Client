623aefa26f5372f834a892396a79b1651ae8504c
nitramSB <martin.brunaes@gmail.com>
2015-07-20 23:59:45 +0200

    First

---
+++ TCP Socket Client/Form1.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.ReplyTextField = New System.Windows.Forms.RichTextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.UsernameLabel = New System.Windows.Forms.Label()
+        Me.TakeUsername = New System.Windows.Forms.Button()
+        Me.UsernameTextbox = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SubmitButton = New System.Windows.Forms.Button()
+        Me.Update = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListViewOnline = New System.Windows.Forms.ListView()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Enabled = False
+        Me.RichTextBox1.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.RichTextBox1.Location = New System.Drawing.Point(16, 196)
+        Me.RichTextBox1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(355, 176)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'ReplyTextField
+        '
+        Me.ReplyTextField.Enabled = False
+        Me.ReplyTextField.Location = New System.Drawing.Point(16, 388)
+        Me.ReplyTextField.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.ReplyTextField.Name = "ReplyTextField"
+        Me.ReplyTextField.Size = New System.Drawing.Size(355, 93)
+        Me.ReplyTextField.TabIndex = 3
+        Me.ReplyTextField.Text = "Test"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Palatino Linotype", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(11, 170)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(101, 22)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Public Chat:"
+        '
+        'UsernameLabel
+        '
+        Me.UsernameLabel.AutoSize = True
+        Me.UsernameLabel.Font = New System.Drawing.Font("Palatino Linotype", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.UsernameLabel.ForeColor = System.Drawing.Color.Black
+        Me.UsernameLabel.Location = New System.Drawing.Point(123, 128)
+        Me.UsernameLabel.Name = "UsernameLabel"
+        Me.UsernameLabel.Size = New System.Drawing.Size(133, 22)
+        Me.UsernameLabel.TabIndex = 14
+        Me.UsernameLabel.Text = "Username status"
+        '
+        'TakeUsername
+        '
+        Me.TakeUsername.Location = New System.Drawing.Point(75, 75)
+        Me.TakeUsername.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.TakeUsername.Name = "TakeUsername"
+        Me.TakeUsername.Size = New System.Drawing.Size(258, 50)
+        Me.TakeUsername.TabIndex = 13
+        Me.TakeUsername.Text = "Take username"
+        Me.TakeUsername.UseVisualStyleBackColor = True
+        '
+        'UsernameTextbox
+        '
+        Me.UsernameTextbox.Location = New System.Drawing.Point(75, 42)
+        Me.UsernameTextbox.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.UsernameTextbox.Name = "UsernameTextbox"
+        Me.UsernameTextbox.Size = New System.Drawing.Size(257, 26)
+        Me.UsernameTextbox.TabIndex = 12
+        Me.UsernameTextbox.Text = "Dave"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(115, 19)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(166, 20)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Choose a nickname"
+        '
+        'SubmitButton
+        '
+        Me.SubmitButton.Enabled = False
+        Me.SubmitButton.Location = New System.Drawing.Point(14, 486)
+        Me.SubmitButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.SubmitButton.Name = "SubmitButton"
+        Me.SubmitButton.Size = New System.Drawing.Size(356, 44)
+        Me.SubmitButton.TabIndex = 10
+        Me.SubmitButton.Text = "Submit"
+        Me.SubmitButton.UseVisualStyleBackColor = True
+        '
+        'Update
+        '
+        Me.Update.Interval = 500
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Palatino Linotype", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(494, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(237, 49)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "Users Online"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(503, 372)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(185, 58)
+        Me.Button1.TabIndex = 18
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListViewOnline
+        '
+        Me.ListViewOnline.Location = New System.Drawing.Point(468, 84)
+        Me.ListViewOnline.Name = "ListViewOnline"
+        Me.ListViewOnline.Size = New System.Drawing.Size(281, 198)
+        Me.ListViewOnline.TabIndex = 20
+        Me.ListViewOnline.UseCompatibleStateImageBehavior = False
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.SubmitButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(872, 540)
+        Me.Controls.Add(Me.ListViewOnline)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.UsernameLabel)
+        Me.Controls.Add(Me.TakeUsername)
+        Me.Controls.Add(Me.UsernameTextbox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.SubmitButton)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ReplyTextField)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "AnonyChat"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents ReplyTextField As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents UsernameLabel As System.Windows.Forms.Label
+    Friend WithEvents TakeUsername As System.Windows.Forms.Button
+    Friend WithEvents UsernameTextbox As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents SubmitButton As System.Windows.Forms.Button
+    Friend WithEvents Update As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ListViewOnline As System.Windows.Forms.ListView
+
+End Class
---
+++ TCP Socket Client/Form1.vb
@@ -0,0 +1,188 @@
+﻿Imports System.Net, System.Net.Sockets
+
+Public Class Form1
+    Inherits System.Windows.Forms.Form
+
+
+
+    Dim byteData(1023) As Byte
+
+    Dim clientSocket = New Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+    Dim ipAddress As IPAddress = IPAddress.Parse("127.0.0.1")
+    Dim ipEndPoint As IPEndPoint = New IPEndPoint(ipAddress, 8800)
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+    End Sub
+    Private Sub OnConnect(ByVal ar As IAsyncResult)
+
+        Try
+            clientSocket.EndConnect(ar)
+            clientSocket.BeginReceive(byteData, 0, byteData.Length, SocketFlags.None, _
+                                      New AsyncCallback(AddressOf OnRecieve), clientSocket)
+
+        Catch ex As Exception
+
+
+        End Try
+       
+    End Sub
+    Private Sub OnRecieve(ByVal ar As IAsyncResult)
+        Try
+            Dim client As Socket = ar.AsyncState
+            client.EndReceive(ar)
+            Dim bytesRec As Byte() = byteData
+            Dim message As String = System.Text.ASCIIEncoding.ASCII.GetString(bytesRec)
+            Read(message)
+            clientSocket.BeginReceive(byteData, 0, byteData.Length, SocketFlags.None, _
+                                      New AsyncCallback(AddressOf OnRecieve), clientSocket)
+            Array.Clear(byteData, 0, byteData.Length)
+
+        Catch ex As Exception
+            MsgBox("Ops, something went wrong. Please try again later")
+            End
+
+        End Try
+        
+    End Sub
+    Delegate Sub _Read(ByVal msg As String)
+    Public socketinfo As String
+
+    Private Sub Read(ByVal msg As String)
+        If InvokeRequired Then
+            Invoke(New _Read(AddressOf Read), msg)
+            Exit Sub
+        End If
+
+        If msg.StartsWith("socketinformation") Then
+
+            Dim MessageArray() As String
+            MessageArray = msg.Split("|")
+            socketinfo = MessageArray(1)
+
+        ElseIf msg.StartsWith("usernameapproved") Then
+            UsernameLabel.Text = UsernameTextbox.Text & " is available!"
+            UsernameLabel.ForeColor = Color.Green
+            SubmitButton.Enabled = True
+            ReplyTextField.Enabled = True
+            TakeUsername.Enabled = False
+            UsernameTextbox.Enabled = False
+
+            Update.Enabled = True
+
+
+        ElseIf msg.StartsWith("usernamenotapproved") Then
+        UsernameLabel.Text = UsernameTextbox.Text & " is not available"
+        UsernameLabel.ForeColor = Color.Red
+
+
+        ElseIf msg.StartsWith("usernameupdate") Then
+        'Do nothing
+        ElseIf msg.StartsWith("heartbeat") Then
+        'Do nothing
+        ElseIf msg.StartsWith("usersonline") Then
+
+
+        Dim MessageArray() As String
+        MessageArray = msg.Split("|")
+        Dim username As String = MessageArray(1)
+            ListViewOnline.Items.Add(username)
+
+        Else
+
+        RichTextBox1.Text = "" & msg
+
+
+        '  RichTextBox1.Text &= msg
+
+        End If
+
+
+    End Sub
+
+   
+    Private Sub Send(ByVal msg As String, ByVal client As Socket)
+        Try
+            Dim sendBytes As Byte() = System.Text.ASCIIEncoding.ASCII.GetBytes(msg)
+
+            client.BeginSend(sendBytes, 0, sendBytes.Length, SocketFlags.None, New AsyncCallback(AddressOf OnSend), client)
+        Catch ex As Exception
+
+        End Try
+
+
+    End Sub
+    Private Sub OnSend(ByVal ar As IAsyncResult)
+        Dim client As Socket = ar.AsyncState
+        client.EndSend(ar)
+    End Sub
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        Try
+            clientSocket.BeginConnect(ipEndPoint, New AsyncCallback(AddressOf OnConnect), Nothing)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+
+
+    Private Sub TakeUsername_Click(sender As Object, e As EventArgs) Handles TakeUsername.Click
+        Try
+            clientSocket.BeginConnect(ipEndPoint, New AsyncCallback(AddressOf OnConnect), Nothing)
+            Send("username|" & UsernameTextbox.Text & "|" & socketinfo, clientSocket)
+
+        Catch ex As Exception
+
+            Application.Restart()
+
+        End Try
+
+
+    End Sub
+
+    Private Sub SubmitButton_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
+        If ReplyTextField.Text = "" Then
+
+        Else
+            Send(UsernameTextbox.Text & "# " & ReplyTextField.Text, clientSocket)
+            ReplyTextField.Text = ""
+
+        End If
+
+
+    End Sub
+
+    Private Sub Update_Tick(sender As Object, e As EventArgs) Handles Update.Tick
+        Try
+            Send("update", clientSocket)
+
+        Catch ex As Exception
+
+        End Try
+
+
+
+    End Sub
+
+    Private Sub Alive_Tick(sender As Object, e As EventArgs)
+        Send("alive", clientSocket)
+    End Sub
+
+   
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs)
+        Me.Size = New System.Drawing.Size(100, 100)
+
+    End Sub
+
+    Private Sub Button1_Click_2(sender As Object, e As EventArgs) Handles Button1.Click
+        Send("usersonline|" & socketinfo, clientSocket)
+
+    End Sub
+End Class
